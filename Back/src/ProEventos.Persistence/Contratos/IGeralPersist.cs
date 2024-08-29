0b2ca256e385b1c2f9065c43ff8b4f88de6a0f2e
Felipe De Andrade <felipe.deam98@gmail.com>
2024-08-29 20:03:15 -0300

    feat: CRUD functions

---
+++ Back/src/ProEventos.Persistence/Contratos/IGeralPersist.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProEventos.Domain;
+
+namespace ProEventos.Persistence.Contratos
+{
+    public interface IGeralPersist
+    {
+        //Geral
+        void Add<T>(T entity) where T : class;
+
+        void Update<T>(T entity) where T : class;
+
+        void Delete<T>(T entity) where T : class;
+
+        void DeleteTange<T>(T[] entity) where T : class;
+
+        Task<bool> SaveChangesAsync();
+    }
+}
